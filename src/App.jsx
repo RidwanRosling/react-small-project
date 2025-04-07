025f831706201e3eb7ef2c1445516fb29204b877
RidwanRosling <ridwansbdt@gmail.com>
2025-04-07 14:40:04 +0700

    learn to create carousel with react

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const arr = [
+  {
+    id: 1,
+    name: "photo1.png",
+    src: "/photo1.png",
+  },
+  {
+    id: 2,
+    name: "photo2.png",
+    src: "/photo2.png",
+  },
+  {
+    id: 3,
+    name: "photo3.png",
+    src: "/photo3.png",
+  },
+];
+
+export default function App() {
+  const [curIndex, setCurIndex] = useState(0);
+  function leftSide() {
+    setCurIndex((prevIndex) =>
+      prevIndex < arr.length - 1 ? 0 : prevIndex - 1
+    );
+  }
+
+  function rightSide() {
+    setCurIndex((prevIndex) =>
+      prevIndex >= arr.length - 1 ? 2 : prevIndex + 1
+    );
+  }
+
+  return (
+    <div className="conatiner">
+      <h1 style={{ display: "flex", justifyContent: "center" }}>
+        {arr[curIndex].name}
+      </h1>
+      <PhotoName
+        gallery={curIndex}
+        changeImgLeft={leftSide}
+        changeImgRight={rightSide}
+      />
+    </div>
+  );
+}
+
+function PhotoName({ gallery, changeImgLeft, changeImgRight }) {
+  return (
+    <div className="photo-name">
+      <button className="btn-1" onClick={changeImgLeft}>
+        👈
+      </button>
+      <img src={arr[gallery].src} alt="" />
+      <button className="btn-2" onClick={changeImgRight}>
+        👉
+      </button>
+    </div>
+  );
+}
